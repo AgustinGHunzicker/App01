077f70e65b4fe1a3693c1ea9ec287a43563c2898
Juani Gerarduzzi <jigerarduzzi@gmail.com>
2020-10-07 12:30:30 -0300

    Se agregó toolbar en lista de platos y se creo una nueva interfaz de usuario (sin validaciones ni funcionalidades)

---
+++ app/src/main/java/com/example/myapplication/PedidoActivity.java
@@ -0,0 +1,20 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+
+public class PedidoActivity extends AppCompatActivity {
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_pedido);
+
+            //Toolbar toolbar = findViewById(R.id.toolbarActivityHome);
+            // setSupportActionBar(toolbar);
+        }
+
+    }
+
--- app/src/main/java/com/example/myapplication/PlateLogic/PlateRecyclerActivity.java
+++ app/src/main/java/com/example/myapplication/PlateLogic/PlateRecyclerActivity.java
@@ -1,18 +1,28 @@
 package com.example.myapplication.PlateLogic;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
+import com.example.myapplication.NuevoPlate;
+import com.example.myapplication.PedidoActivity;
 import com.example.myapplication.R;
+import com.example.myapplication.RegistrarUsuario;
 
 public class PlateRecyclerActivity extends AppCompatActivity {
     PlatesDao daoPlates;
     private RecyclerView recyclerView;
     private RecyclerView.Adapter plateAdapter;
     private RecyclerView.LayoutManager layoutManager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,5 +35,33 @@ public class PlateRecyclerActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(layoutManager);
         plateAdapter = new PlateAdapter(daoPlates.list());//,this);
         recyclerView.setAdapter(plateAdapter);
+
+        //super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_home);
+
+        Toolbar toolbar = findViewById(R.id.toolbarPlateRecycler);
+        setSupportActionBar(toolbar);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_lista_items,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Resources res = getResources();
+        switch (item.getItemId()){
+            case R.id.menuListOption1:
+                Intent registerScreen = new Intent(this, PedidoActivity.class);
+                startActivity(registerScreen);
+                Log.d("New","ELIGIO NUEVO PEDIDO");
+
+            default:
+                //error desconocido
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
