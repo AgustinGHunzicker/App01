8a6f999a384c1e0f3e09a76bbd721a8f90e6cf6a
Agustin <agustingonzaleshunzicker@gmail.com>
2020-10-17 12:21:25 -0300

    activity registrar xml

--- app/src/main/java/com/example/myapplication/activity/ActivityRegistrarUsuario.java
+++ app/src/main/java/com/example/myapplication/activity/ActivityRegistrarUsuario.java
@@ -30,7 +30,7 @@ public class ActivityRegistrarUsuario extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_registrar);
 
         /*
               EditText variables
