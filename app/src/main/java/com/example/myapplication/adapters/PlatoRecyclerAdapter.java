2e19f4b6301f07d2be0c9ca4d640f07b3d9c9012
Agustin <agustingonzaleshunzicker@gmail.com>
2020-10-17 21:26:59 -0300

    PlatoRecyclerAdapter --> unificado con el holder

--- app/src/main/java/com/example/myapplication/adapters/PlatoRecyclerAdapter.java
+++ app/src/main/java/com/example/myapplication/adapters/PlatoRecyclerAdapter.java
@@ -1,19 +1,21 @@
 package com.example.myapplication.adapters;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
-
 import com.example.myapplication.R;
 import com.example.myapplication.activity.ActivityPedido;
 import com.example.myapplication.activity.ActivityPlateRecycler;
 import com.example.myapplication.model.Plato;
-
 import java.util.List;
 
 /*
@@ -22,30 +24,72 @@ import java.util.List;
  - El adaptador permite el acceso a los elementos de datos
  - Este también es responsable de crear una vista para cada elemento en la colección de datos.
  */
-public class PlatoRecyclerAdapter extends RecyclerView.Adapter<ViewHolderPlato> {
+public class PlatoRecyclerAdapter extends RecyclerView.Adapter<PlatoRecyclerAdapter.ViewHolderPlato> {
 
-    private ActivityPlateRecycler activityPlateRecycler;
-    private List<Plato> plateList;
-    private Boolean addButtonAsk;
+    private final ActivityPlateRecycler activityPlateRecycler;
+    private final List<Plato> plateList;
+    private final Boolean addButtonAsk;
 
-    public PlatoRecyclerAdapter(ActivityPlateRecycler activityPlateRecycler, List<Plato> plateListParameter, Boolean addButtonAsk){
+    public PlatoRecyclerAdapter(ActivityPlateRecycler activityPlateRecycler,
+                                List<Plato> plateListParameter, Boolean addButtonAsk){
+
         this.activityPlateRecycler = activityPlateRecycler;
         this.plateList = plateListParameter;
         this.addButtonAsk = addButtonAsk;
     }
 
     /*
-    -   Este método se ejecutará UNA VEZ por cada fila que se visualiza.
-    -   Obtenemos una vista y a partir de la misma la inflamos con el layout
-     */
+        Holder
+        Tendrá como atributos, punteros a instancias de los distintos View que muestra una fila.
+    */
+    public static class ViewHolderPlato extends RecyclerView.ViewHolder {
+
+        ImageView plateImage;
+        CardView cardViewPlate;
+        TextView textTitle;
+        TextView textDescription;
+        TextView textPrice;
+        TextView textCalories;
+        Button btnVer;
+        Button btnPedirPlato;
+        Plato plate;
+
+        public ViewHolderPlato(@NonNull View itemView, Boolean addButttonAsk) {
+            super(itemView);
+            //this.plateImage = (ImageView) itemView.findViewById(R.id.);
+            this.cardViewPlate = itemView.findViewById(R.id.cardViewPlate);
+            this.textTitle = itemView.findViewById(R.id.textTitlePlate);
+            //this.textDescription = (TextView) itemView.findViewById(R.id.textDescriptionPlate);
+            this.textPrice = (TextView) itemView.findViewById(R.id.textPricePlate);
+            //this.textCalories = (TextView) itemView.findViewById(R.id.textCaloriesPlate);
+            this.btnVer = (Button) itemView.findViewById(R.id.btnVer);
+            this.btnPedirPlato = (Button) itemView.findViewById(R.id.btnAsk);
+
+            if(addButttonAsk){
+                btnPedirPlato.setVisibility(View.VISIBLE);
+                btnVer.setVisibility(View.GONE);
+            }
+            else{
+                btnVer.setVisibility(View.VISIBLE);
+                btnPedirPlato.setVisibility(View.GONE);
+            }
+        }
+    }
+
+    /*
+-   Este método se ejecutará UNA VEZ por cada fila que se visualiza.
+-   Obtenemos una vista y a partir de la misma la inflamos con el layout
+ */
+    @NonNull
     public ViewHolderPlato onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.fila_plato, parent, false);
         return new ViewHolderPlato(view, addButtonAsk);
     }
 
     /*
-        El adaptador vincula los ViewHolderPlato con Plato
-     */
+    El adaptador vincula los ViewHolderPlato con Plato
+ */
+    @SuppressLint("SetTextI18n")
     public void onBindViewHolder(@NonNull final ViewHolderPlato plateHolder, int position) {
         plateHolder.plate = plateList.get(position);
         plateHolder.cardViewPlate.setTag(position);
