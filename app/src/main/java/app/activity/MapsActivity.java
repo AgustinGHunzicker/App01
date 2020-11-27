f74c004a01cb18b892ef011c194eae36ad83082a
Juani Gerarduzzi <jigerarduzzi@gmail.com>
2020-11-27 08:02:00 -0300

    Lista la parte de mapas. Falta retornar la ubicación desde activity_maps a activity_pedido

--- app/src/main/java/app/activity/MapsActivity.java
+++ app/src/main/java/app/activity/MapsActivity.java
@@ -1,21 +1,33 @@
 package app.activity;
 
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.os.Bundle;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polygon;
+import com.google.android.gms.maps.model.PolygonOptions;
+import com.google.maps.android.SphericalUtil;
+
+import java.util.Random;
 
 import SendMeal.app.R;
+
+import static android.graphics.Color.RED;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    LatLng puntoPedido;
+    LatLng puntoLocal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,10 +51,60 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        actualizarMapa();
+        mMap.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
+            @Override
+            public void onMapLongClick(LatLng latLng) {
+                mMap.clear();
+                puntoPedido = latLng;
+                mMap.addMarker(new MarkerOptions().position(puntoPedido).title("Punto Pedido"));
+                puntoLocal = generarPosicionLocal(puntoPedido);
+                mMap.addMarker(new MarkerOptions().position(puntoLocal).title("Punto Local") );
 
+                PolygonOptions polylineOptions = new PolygonOptions()
+                        .add(puntoLocal).add(puntoPedido);
+                Polygon p = mMap.addPolygon(polylineOptions);
+                p.setStrokeColor(RED);
+            }
+        });
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+       // LatLng sydney = new LatLng(-34, 151);
+       // mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+       // mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
+
+    private void actualizarMapa(){
+        if (ActivityCompat.checkSelfPermission (this , Manifest.permission.ACCESS_FINE_LOCATION)
+                !=PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this,Manifest.permission.ACCESS_COARSE_LOCATION)
+                !=PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    9999);
+            return;
+        }
+        mMap.setMyLocationEnabled(true);
+
+    }
+
+    public LatLng generarPosicionLocal (LatLng posicionOriginal){
+        Random r = new Random();
+
+// Una direccion aleatoria de 0 a 359 grados
+        int direccionRandomEnGrados = r.nextInt(360);
+
+// Una distancia aleatoria de 100 a 1000 metros
+        int distanciaMinima = 100;
+        int distanciaMaxima = 1000;
+        int distanciaRandomEnMetros = r.nextInt(distanciaMaxima - distanciaMinima) + distanciaMinima;
+
+        LatLng nuevaPosicion = SphericalUtil.computeOffset(
+                posicionOriginal,
+                distanciaRandomEnMetros,
+                direccionRandomEnGrados
+        );
+
+        return nuevaPosicion;
+    }
+
+
 }
